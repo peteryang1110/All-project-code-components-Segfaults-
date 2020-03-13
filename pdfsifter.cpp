cd3462276f33741223ed0d9d35db5b9263e9857a
picabotpi <yumi4478@colorado.edu>
2020-03-13 14:44:36 -0600

    Added csv

---
+++ pdfsifter.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool subStrValidate(string line){
+    if(line.substr())
+}
+
+
+int readData(string filename){
+    ifstream file;
+    string line = "";
+    file.open(filename);
+    while(getline(file, line)){
+        if(line.length() > 4 && ){
+            
+        }
+    }
+
+}
+
+void outputFile(Arr){
+    
+    for (int i = 0; i < 13; i++) 
+    { 
+        for (int j = 0; j < 13 ; j++) 
+        { 
+            Arr[i][j]
+        } 
+    } 
+  
+    return 0; 
+} 
+
+}
