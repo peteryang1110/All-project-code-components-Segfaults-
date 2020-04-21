7fca317adbc790b55a308271ef7536318e912a34
MaxwellBanks <56692967+MaxwellBanks@users.noreply.github.com>
2020-04-21 14:12:46 -0600

    Fixing Query

--- sql_files/queries.sql
+++ sql_files/queries.sql
@@ -4,7 +4,6 @@
 -- NOTE! IF A VALUE IS NULL, change it to %
 var course_code_input;
 var course_subject_input;
-var course_professor_input;
 
 SELECT Class_info.Course_department, Class_info.Course_subject, 
     Class_info.Course_start, Class_info.Course_end, Class_info.Course_instructor, 
@@ -15,9 +14,7 @@ SELECT Class_info.Course_department, Class_info.Course_subject,
     AND Class_info.Course_subject = Course_info.Course_subject
     WHERE
         Class_info.Course_department LIKE '%' || course_code_input || '%'
-        AND(CAST(Class_info.Course_subject AS VARCHAR(5)) LIKE '%' || course_subject_input || '%')
-        AND(Class_info.Course_instructor LIKE '%' || course_professor_input || '%');
-
+        AND(CAST(Class_info.Course_subject AS VARCHAR(5)) LIKE '%' || course_subject_input || '%');
 -- Example query
 SELECT Class_info.Course_department, Class_info.Course_subject, 
     Class_info.Course_start, Class_info.Course_end, Class_info.Course_instructor, 
@@ -28,5 +25,19 @@ SELECT Class_info.Course_department, Class_info.Course_subject,
     AND Class_info.Course_subject = Course_info.Course_subject
     WHERE
         Class_info.Course_department LIKE '%' || 'CSCI' || '%'
-        AND(CAST(Class_info.Course_subject AS VARCHAR(5)) LIKE '%' || 1300 || '%')
-        AND(Class_info.Course_instructor LIKE '%' || '%' || '%');
+        AND(CAST(Class_info.Course_subject AS VARCHAR(5)) LIKE '%' || 1300 || '%');
+
+-- Final Result
+var course_code_input = input();
+var course_subject_input = input();
+
+var search_result = "SELECT Class_info.Course_department, Class_info.Course_subject, 
+    Class_info.Course_start, Class_info.Course_end, Class_info.Course_instructor, 
+    Class_info.Course_days, Course_info.Course_name
+    FROM Class_info
+    FULL OUTER JOIN Course_info 
+    ON Class_info.Course_department = Course_info.Course_department
+    AND Class_info.Course_subject = Course_info.Course_subject
+    WHERE
+        Class_info.Course_department LIKE '%' ||" +  course_code_input + "|| '%'
+        AND(CAST(Class_info.Course_subject AS VARCHAR(5)) LIKE '%' ||" + course_subject_input + "|| '%');"
