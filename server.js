d7fa935263a7fef6a97a6b2bfa11d7e551016a9e
Yun-Ping Yang <yuya0723@colorado.edu>
2020-04-21 20:42:05 +0800

    Testing to run on a server

---
+++ server.js
@@ -0,0 +1,27 @@
+/***********************
+  Load Components!
+
+  Express      - A Node.js Framework
+  Body-Parser  - A tool to help use parse the data in a post request
+  Pg-Promise   - A database tool to help use connect to our PostgreSQL database
+***********************/
+var express = require("express"); //Ensure our express framework has been added
+var app = express();
+var bodyParser = require("body-parser"); //Ensure our body-parser tool has been added
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//Create Database Connection
+// var pgp = require("pg-promise")();
+// const dbConfig = process.env.DATABASE_URL;
+// var db = pgp(dbConfig);
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/")); //This line is necessary for us to use relative paths and access our resources directory
+
+app.get("/", function (req, res) {
+  res.render("/index", {});
+});
+
+app.listen(3000);
